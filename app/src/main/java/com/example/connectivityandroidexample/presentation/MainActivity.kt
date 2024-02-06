25c6747323fc9901306535e5aec9083e6e8d80dc
Fabrizio Bertoglio <fabrizio.bertoglio@gmail.com>
2024-02-06 16:23:14 +0800

    clean up code

--- app/src/main/java/com/example/connectivityandroidexample/presentation/MainActivity.kt
+++ app/src/main/java/com/example/connectivityandroidexample/presentation/MainActivity.kt
@@ -16,6 +16,8 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.lifecycleScope
+import com.google.android.gms.tasks.OnFailureListener
+import com.google.android.gms.tasks.OnSuccessListener
 import com.google.android.gms.wearable.MessageClient
 import com.google.android.gms.wearable.MessageEvent
 import com.google.android.gms.wearable.Node
@@ -27,8 +29,7 @@ import java.util.concurrent.ExecutionException
 
 
 class MainActivity : ComponentActivity(), MessageClient.OnMessageReceivedListener {
-
-    // private lateinit var mobileDeviceListener: MobileDeviceListener
+    private var nodes: MutableList<Node>? = null
     var count by mutableStateOf(0)
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,18 +37,16 @@ class MainActivity : ComponentActivity(), MessageClient.OnMessageReceivedListene
 
         super.onCreate(savedInstanceState)
         Wearable.getMessageClient(this).addListener(this)
-        /*
-        mobileDeviceListener = MobileDeviceListener(this) { node ->
-            // update UI
-        }
-         */
         lifecycleScope.launch(Dispatchers.Main) {
-            val result = getNodes();
-            if (result != null) {
-                onResult(result)
+            nodes = getNodes();
+            if (nodes!!.size > 0) {
+                for (node in nodes) {
+                    if (nodes != null) {
+                        sendMessageToClient(node)
+                    }
+                }
             }
         }
-
 
         setContent {
             WearApp(currentCount = count, { increaseCount() })
@@ -57,11 +56,7 @@ class MainActivity : ComponentActivity(), MessageClient.OnMessageReceivedListene
     suspend fun getNodes(): MutableList<Node>? {
         val nodeClient = Wearable.getNodeClient(this);
         try {
-            // Block on a task and get the result synchronously. This is generally done
-            // when executing a task inside a separately managed background thread. Doing this
-            // on the main (UI) thread can cause your application to become unresponsive.
             val result = nodeClient.getConnectedNodes().await();
-            Log.w("Testing: ", "result: " + result);
             return result;
         } catch (e: ExecutionException) {
             // The Task failed, this is the same exception you'd get in a non-blocking
@@ -78,27 +73,6 @@ class MainActivity : ComponentActivity(), MessageClient.OnMessageReceivedListene
 
     private fun onResult(result: MutableList<Node>?) {
         Log.w("TESTING: ", "result: " + result);
-        /*if (result.size > 0) {
-            for (node in nodes) {
-                sendMessageToClient(node)
-            }
-        }*/
-
-        /*
-        try {
-            val sendTask = Wearable.getMessageClient(applicationContext).sendMessage(
-                node.getId(), "/increase_wear_counter", null
-            )
-            val onSuccessListener: OnSuccessListener<Any> =
-                OnSuccessListener { Log.w("TESTING ", "onSuccess") }
-            sendTask.addOnSuccessListener(onSuccessListener)
-            val onFailureListener =
-                OnFailureListener { e -> Log.w("TESTING ", "onFailure with e: $e") }
-            sendTask.addOnFailureListener(onFailureListener)
-        } catch (e: Exception) {
-            Log.w("TESTING", "e $e")
-        }
-        */
     }
 
     public override fun onStart() {
@@ -118,12 +92,23 @@ class MainActivity : ComponentActivity(), MessageClient.OnMessageReceivedListene
     }
 
     fun sendMessageToClient(node: Node) {
-        Log.w("TESTING: ", "sendMessageToClient");
+        try {
+            val sendTask = Wearable.getMessageClient(applicationContext).sendMessage(
+                node.getId(), "/increase_wear_counter", null
+            )
+            val onSuccessListener: OnSuccessListener<Any> =
+                OnSuccessListener { Log.w("TESTING from wear: ", "onSuccess") }
+            sendTask.addOnSuccessListener(onSuccessListener)
+            val onFailureListener =
+                OnFailureListener { e -> Log.w("TESTING from wear: ", "onFailure with e: $e") }
+            sendTask.addOnFailureListener(onFailureListener)
+        } catch (e: Exception) {
+            Log.w("TESTING from wear: ", "e $e")
+        }
     }
 
     fun onConnected(@Nullable bundle: Bundle?) {
         Wearable.getMessageClient(this).addListener(this)
-        // InitNodesTask().execute(this.applicationContext)
     }
 
 
@@ -133,23 +118,3 @@ class MainActivity : ComponentActivity(), MessageClient.OnMessageReceivedListene
         }
     }
 }
-
-// https://developer.android.com/topic/libraries/architecture/lifecycle#kotlin
-// https://github.com/bevkoski/react-native-android-wear-demo/blob/efcbc7973e094472a75d7e27ec305431d2eb2fc3/android/wear/src/main/java/com/reactnativeandroidweardemo/MainActivity.java#L78
-/*
-internal class MobileDeviceListener(
-    private val context: Context,
-    private val callback: (Node) -> Unit
-) {
-
-    fun start() {
-        Log.w("TESTING: ", "mobileDeviceListener.start()");
-        // finish the implementation
-        // https://developer.android.com/topic/libraries/architecture/lifecycle#kotlin
-    }
-
-    fun stop() {
-        Log.w("TESTING: ", "mobileDeviceListener.stop()");
-    }
-}
- */
