ab86f0382fc410a4134ca903f63482927a218f0c
Fabrizio Bertoglio <fabrizio.bertoglio@gmail.com>
2025-02-21 14:47:19 +0800

    ask for permissions to record audio

--- app/src/main/java/com/wearconnectivityexample/ui/MainActivity.kt
+++ app/src/main/java/com/wearconnectivityexample/ui/MainActivity.kt
@@ -3,6 +3,11 @@ package com.example.wearconnectivityexample.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import android.Manifest
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import android.app.Activity
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,5 +15,17 @@ class MainActivity : ComponentActivity() {
         setContent {
             WearApp()
         }
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        requestAudioPermission(this);
+    }
+}
+
+fun requestAudioPermission(activity: Activity) {
+    if (ContextCompat.checkSelfPermission(activity, Manifest.permission.RECORD_AUDIO)
+        != PackageManager.PERMISSION_GRANTED) {
+        ActivityCompat.requestPermissions(activity, arrayOf(Manifest.permission.RECORD_AUDIO), 100)
     }
 }
