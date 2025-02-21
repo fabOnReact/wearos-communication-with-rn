e3613f725b0e47a69c5ba3c0fde460c7b2202145
Fabrizio Bertoglio <fabrizio.bertoglio@gmail.com>
2025-02-21 13:56:12 +0800

    scaling lazy column will correctly re-size items at the beginning and end of the list

--- app/src/main/java/com/wearconnectivityexample/ui/screens/ChatListScreen.kt
+++ app/src/main/java/com/wearconnectivityexample/ui/screens/ChatListScreen.kt
@@ -1,11 +1,14 @@
 package com.wearconnectivityexample.ui.screens
 
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.wear.compose.foundation.lazy.ScalingLazyColumn
+import androidx.wear.compose.foundation.lazy.ScalingLazyColumnDefaults
 import androidx.wear.compose.material.Chip
 import androidx.wear.compose.material.ChipDefaults
 import androidx.wear.compose.material.Text
@@ -15,13 +18,17 @@ import androidx.wear.compose.foundation.lazy.rememberScalingLazyListState
 fun ChatListScreen(
     onChatSelected: (String) -> Unit
 ) {
-    val chatList = listOf("08123111717", "08123111715")
+    val chatList = listOf("08123111717", "08123111715", "08123111715", "08123111715", "08123111715")
     val listState = rememberScalingLazyListState()
 
     // This is the Wear-specific lazy column with scaling
     ScalingLazyColumn(
         modifier = Modifier.fillMaxSize(),
         state = listState,
+        scalingParams = ScalingLazyColumnDefaults.scalingParams(
+            edgeScale = 0.015f,  // how much items shrink near the edge
+            minTransitionArea = 0.24f,
+        )
     ) {
         items(chatList.size) { index ->
             val phoneNumber = chatList[index]
