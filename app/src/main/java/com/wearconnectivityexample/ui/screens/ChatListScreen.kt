41fc62581980e301722c51375f79004463dff96b
Fabrizio Bertoglio <fabrizio.bertoglio@gmail.com>
2025-08-29 10:32:04 +0200

    chore: clean up screen imports

--- app/src/main/java/com/wearconnectivityexample/ui/screens/ChatListScreen.kt
+++ app/src/main/java/com/wearconnectivityexample/ui/screens/ChatListScreen.kt
@@ -1,10 +1,7 @@
+
 package com.wearconnectivityexample.ui.screens
 
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ChatBubble
 import androidx.compose.runtime.Composable
@@ -18,7 +15,6 @@ import androidx.wear.compose.material.Chip
 import androidx.wear.compose.material.ChipDefaults
 import androidx.wear.compose.material.Icon
 import androidx.wear.compose.material.Text
-import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.filled.ArrowUpward
 import androidx.compose.ui.Alignment
