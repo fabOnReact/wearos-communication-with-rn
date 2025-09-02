e6f49cd0c6181c08cf4699164742ad6809835179
Fabrizio Bertoglio <fabrizio.bertoglio@gmail.com>
2025-09-02 17:28:29 +0200

    refactor: consolidate audio recording

--- app/src/main/java/com/wearconnectivityexample/ui/WearApp.kt
+++ app/src/main/java/com/wearconnectivityexample/ui/WearApp.kt
@@ -7,7 +7,7 @@ import androidx.wear.compose.navigation.SwipeDismissableNavHost
 import androidx.wear.compose.navigation.composable
 import androidx.navigation.navArgument
 import com.example.mywearosapp.ui.screens.ChatDetailScreen
-import com.example.wearconnectivityexample.ui.screens.RecordVoiceScreen
+import com.wearconnectivityexample.ui.screens.RecordVoiceScreen
 import com.wearconnectivityexample.ui.screens.ChatListScreen
 
 @Composable
--- app/src/main/java/com/wearconnectivityexample/ui/screens/RecordVoiceScreen.kt
+++ app/src/main/java/com/wearconnectivityexample/ui/screens/RecordVoiceScreen.kt
@@ -1,4 +1,4 @@
-package com.example.wearconnectivityexample.ui.screens
+package com.wearconnectivityexample.ui.screens
 
 import android.content.Context
 import android.media.MediaRecorder
@@ -6,8 +6,6 @@ import android.util.Log
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
@@ -16,26 +14,22 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.unit.dp
-import androidx.wear.compose.material.Chip
-import androidx.wear.compose.material.ChipDefaults
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.wear.compose.material.Icon
 import androidx.wear.compose.material.Text
 import androidx.wear.compose.material.ToggleButton
 import androidx.wear.compose.material.ToggleButtonDefaults
 import com.google.android.gms.wearable.Asset
+import com.google.android.gms.wearable.DataMap
 import com.google.android.gms.wearable.PutDataMapRequest
 import com.google.android.gms.wearable.Wearable
+import com.wearconnectivityexample.R
 import java.io.File
 import java.io.FileInputStream
 import java.io.IOException
-import androidx.wear.compose.material.Icon
-import androidx.wear.compose.material.ToggleButton
-import com.wearconnectivityexample.R
-import androidx.wear.compose.material.Text
-import com.google.android.gms.wearable.DataMap
 
 @Composable
 fun RecordVoiceScreen(
